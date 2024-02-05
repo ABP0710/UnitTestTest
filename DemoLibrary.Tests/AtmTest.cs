a9804bf7b529f18e6194d96cf85aa589869a1a77
ABP0710 <110819363+ABP0710@users.noreply.github.com>
2024-02-05 16:19:24 +0100

    Add project files.

---
+++ DemoLibrary.Tests/AtmTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+using UnitTestTest;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class AtmTest
+    {
+        [Theory]
+        [InlineData(1234, 1234)]
+        [InlineData(6543, 6543)]
+        public void Pinnumber_PinnumberShouldBeEquel(int input, int pin)
+        {
+            // Arrange
+            int expected = pin;
+
+            // Act
+            int actual = Atm.Pinnumber(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1234, 12343)]
+        [InlineData(6543.25, 65433.75)]
+        public void Withdraw_WithdrawShouldBeLessThanAmount(int withdraw, int amount)
+        {
+            // Arrange
+            decimal expetet = amount;
+
+            // Act
+            decimal actual = Atm.Withdraw(withdraw, amount);
+
+            //Assert
+            Assert.True((decimal)expetet >= actual);
+            
+        }
+
+    }
+}
---
+++ UnitTestTest/Atm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestTest
+{
+    public class Atm
+    {
+        public static int Pinnumber(int pressedPinnumber)
+        {
+
+            return pressedPinnumber;
+        }
+        public static decimal Withdraw(decimal amountWithdraw, decimal amountTotal)
+        {
+            return amountWithdraw - amountTotal;
+        }
+    }
+}
